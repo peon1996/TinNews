88e769834ee24f15e9096ac2e4e33978ac8edf6f
Xiaoyang Miao <peon@XiaoyangdeMacBook-Pro.local>
2019-04-24 00:26:25 -0700

    update

---
+++ app/src/main/java/com/company/homework9/activity/splash.java
@@ -0,0 +1,32 @@
+package com.company.homework9.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.company.homework9.R;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(splash.this,MainActivity.class);
+                splash.this.startActivity(mainIntent);
+                splash.this.finish();
+            }
+        }, 1500);
+    }
+
+}
